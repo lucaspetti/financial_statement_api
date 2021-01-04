fff5a2631fb3e472246ccf9a0806ae8be5df06cb
lucaspetti <luc_petti@hotmail.com>
2021-01-04 20:24:56 +0100

    Add request tests for index and create

--- spec/financial_statement_api_spec.rb
+++ spec/financial_statement_api_spec.rb
@@ -7,18 +7,70 @@ RSpec.describe FinancialStatementApi do
     FinancialStatementApi
   end
 
+  let(:json_response) do
+    {
+      "_id"=>statement.id,
+      "date"=>statement.date,
+      "gross_profit"=>nil,
+      "revenue"=>nil
+    }
+  end
+
   describe 'GET /financial_statements' do
-    before do
-      FinancialStatement.create!(date: DateTime.now)
-      get '/financial_statements'
+    context 'when there are no records on the DB' do
+      before { get '/financial_statements' }
+
+      it 'returns an empty JSON response' do
+        expect(last_response).to be_ok
+        expect(last_response.body).to eq("[]")
+      end
     end
 
-    it 'returns a JSON response' do
-      expect(last_response).to be_ok
+    context 'when there are records on the DB' do
+      let!(:statement) { FinancialStatement.create!(date: '2020-20-12') }
+
+      before { get '/financial_statements' }
+
+      it 'returns a JSON response' do
+        expect(last_response).to be_ok
+      end
+
+      it 'returns all financial statements' do
+        expect(last_response.body).to eq([json_response].to_json)
+      end
+    end
+  end
+
+  describe 'POST /financial_statements' do
+    let(:request) { post '/financial_statements', params: params }
+
+    context 'when params are correct' do
+      let(:params) { { financial_statement: { date: '2020-20-12' } } }
+      let(:statement) { FinancialStatement.first }
+
+      it 'creates a financial statement' do
+        expect { request }.to change(FinancialStatement, :count).by(1)
+      end
+
+      it 'returns the created statement it in JSON format' do
+        request
+        expect(last_response).to be_ok
+        expect(last_response.body).to eq(json_response.to_json)
+      end
     end
 
-    it 'returns all financial statements' do
-      expect(last_response.body).to eq(FinancialStatement.all.to_json)
+    context 'when params are incorrect' do
+      let(:params) { {} }
+
+      it 'does not create a financial statement' do
+        expect { request }.not_to change(FinancialStatement, :count)
+      end
+
+      it 'returns the created statement it in JSON format' do
+        request
+        expect(last_response).not_to be_ok
+        expect(last_response.body).to eq('Could not create record')
+      end
     end
   end
 end
